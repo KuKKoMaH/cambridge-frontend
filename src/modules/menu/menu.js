aeddf096f5ddeff81cd0db8824b01047ce40623d
KuKKoMaH <kukkoman@gmail.com>
2019-03-26 21:55:25 +0300

    init

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -12,28 +12,46 @@ new SmoothScroll('.menu__items a', {
   header: '.menu__wrapper',
 });
 
-const $html = $('html');
+// const $html = $('html');
+// const $wrapper = $('.menu__wrapper');
+// const menuHeight = 84;
+// let prevPosition = $html.scrollTop();
+// let initialPosition = prevPosition - menuHeight;
+// let lastDirection = 1;
+// $(window).on('scroll', () => {
+//   const currentPosition = $html.scrollTop();
+//   const diff = currentPosition - prevPosition;
+//   if (!diff) return;
+//   prevPosition = currentPosition;
+//   const direction = diff > 0 ? 1 : -1;
+//   const offset = direction > 0
+//     ? Math.min(menuHeight, currentPosition - initialPosition)
+//     : Math.max(0, menuHeight - (initialPosition - currentPosition));
+//   // console.log(offset);
+//   if (direction !== lastDirection) {
+//     // console.log('dir');
+//     // initialPosition = menuHeight + currentPosition - initialPosition;
+//     initialPosition = currentPosition;
+//     lastDirection = direction;
+//   } else {
+//     $wrapper.css({ top: -offset + 'px' });
+//   }
+// });
+
 const $wrapper = $('.menu__wrapper');
-const menuHeight = 84;
-let prevPosition = $html.scrollTop();
-let initialPosition = prevPosition - menuHeight;
-let lastDirection = 1;
+const $html = $('html');
+let menuActive = false;
 $(window).on('scroll', () => {
-  const currentPosition = $html.scrollTop();
-  const diff = currentPosition - prevPosition;
-  if (!diff) return;
-  prevPosition = currentPosition;
-  const direction = diff > 0 ? 1 : -1;
-  const offset = direction > 0
-    ? Math.min(menuHeight, currentPosition - initialPosition)
-    : Math.max(0, menuHeight - (initialPosition - currentPosition));
-  // console.log(offset);
-  if (direction !== lastDirection) {
-    // console.log('dir');
-    // initialPosition = menuHeight + currentPosition - initialPosition;
-    initialPosition = currentPosition;
-    lastDirection = direction;
+  const scrollTop = $html.scrollTop();
+  if(scrollTop > 10) {
+    if(!menuActive) {
+      $wrapper.addClass('menu__wrapper--active');
+      menuActive = true;
+    }
   } else {
-    $wrapper.css({ top: -offset + 'px' });
+    if(menuActive) {
+      $wrapper.removeClass('menu__wrapper--active');
+      menuActive = false;
+    }
   }
 });
