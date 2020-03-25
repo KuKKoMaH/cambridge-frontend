b70bdd7850be9e32b7676b6ec798dcd20168323b
KuKKoMaH <kukkoman@gmail.com>
2020-03-25 23:03:38 +0300

    фиксы

--- src/modules/contacts/contacts.js
+++ src/modules/contacts/contacts.js
@@ -1,5 +1,5 @@
 import scriptLoader from '../../js/scriptLoader';
-import Breakpoints from 'breakpoints-js';
+import Breakpoints  from 'breakpoints-js';
 import ScrollReveal from 'scrollreveal';
 
 let loaded = false;
@@ -24,14 +24,21 @@ const initMap = () => {
   if (!ymaps) return;
   destroyMap();
   map = new ymaps.Map("map", {
-    center: window.MAP_CENTER,
-    zoom:   window.MAP_ZOOM
+    center: window.MAP_MARKERS[0][0],
+    zoom:   window.MAP_ZOOM,
   });
 
-  map.geoObjects.add(new ymaps.Placemark(window.MAP_CENTER, {}))
+  for (let i = 0; i < window.MAP_MARKERS.length; i++) {
+    var marker = window.MAP_MARKERS[i];
+    map.geoObjects.add(new ymaps.Placemark(marker[0], {
+      balloonContent: marker[1],
+    }));
+  }
+
+  map.setBounds(map.geoObjects.getBounds());
 };
 
-window.INIT_MAP = (a) => {
+window.INIT_MAP = ( a ) => {
   ymaps = a;
   initMap();
 };
@@ -40,17 +47,17 @@ Breakpoints.on('sm', 'enter', () => {
   destroyMap();
   ScrollReveal().clean('.contacts__col, .contacts__col2');
   ScrollReveal().reveal('.contacts__col, .contacts__col2', {
-    distance:    '50px',
-    viewFactor:  .2,
-  })
+    distance:   '50px',
+    viewFactor: .2,
+  });
 });
 Breakpoints.on('lg', 'enter', () => {
   ScrollReveal().clean('.contacts__col, .contacts__col2');
   ScrollReveal().reveal('.contacts__col, .contacts__col2', {
     distance:    '50px',
     viewFactor:  .2,
-    afterReveal: (el) => {
+    afterReveal: ( el ) => {
       if ($(el).find('#map').length) initMap();
     },
-  })
+  });
 });
